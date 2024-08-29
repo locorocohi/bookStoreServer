a1113f1bddeb0da31e07771d7e44009a8491af54
Artem Rudov <dovakin2281337@gmail.com>
2024-08-29 13:55:36 +0300

    added logic for obtaining query parameters for filtering books

--- src/controllers/bookControllers/getAllBooks.ts
+++ src/controllers/bookControllers/getAllBooks.ts
@@ -3,11 +3,19 @@ import type { RequestHandler } from "express"
 import { genres, sortOptions } from "../../services/bookServices";
 
 export const getAllBooks: RequestHandler = async (req, res, next) => {
-  console.log(req.query);
+  const { genre, sortOption, minPrice, maxPrice } = req.query;
+  console.log(genre, sortOption, minPrice, maxPrice)
   const booksArray = await booksRepo.find({
     order: { 
     id: 'ASC'
   }});
   
+  // const booksArray = await booksRepo
+  //   .createQueryBuilder("book")
+  //   .where("book.name= :name", {name: "req.name"})
+  //   .andWhere("book.author= :author", {author: "req.author"})
+  //   .andWhere("book.genre IN (:...genres)", {genres: [...req.genres]})
+  //   .andWhere("book.price= :price")
+
   res.status(200).json({ booksArray, sortOptions, genres });
 }
