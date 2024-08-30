df8f2feb1fb059ed63fdff67ba735ec0069e8d95
Artem Rudov <dovakin2281337@gmail.com>
2024-08-30 13:15:20 +0300

    add logic for pagination on server

--- src/controllers/bookControllers/getBooks.ts
+++ src/controllers/bookControllers/getBooks.ts
@@ -1,50 +1,72 @@
-import { booksRepo } from "../../database"
-import type { RequestHandler } from "express"
+import { booksRepo } from "../../database";
+import type { RequestHandler } from "express";
 import { genres, sortOptions } from "../../services/bookServices";
 import { Book } from "../../database/entity/Book";
 
 type FiltersType = {
-  genre?: string; 
+  genre?: string;
   sortOption?: string;
   minPrice?: string;
   maxPrice?: string;
-}
+  page?: string;
+};
 
 type ResponseType = {
-  booksArray: Book[],
-  sortOptions: string[],
+  booksArray: Book[];
+  sortOptions: string[];
   genres: string[];
-}
+  pageCount: number;
+};
 
-type BooksGetter = RequestHandler<Record<string, unknown>, ResponseType, Record<string, unknown>, FiltersType>;
+type BooksGetter = RequestHandler<
+  Record<string, unknown>,
+  ResponseType,
+  Record<string, unknown>,
+  FiltersType
+>;
 
-export const getBooks: BooksGetter = async (req, res, next) => { // params , res.body, req.body
-  
-  const { genre, sortOption, minPrice, maxPrice } = req.query;
+export const getBooks: BooksGetter = async (req, res, next) => {
+  const { genre, sortOption, minPrice, maxPrice, page } = req.query;
 
-  if (!genre && !sortOption && !minPrice && !maxPrice) {
-    const result = await booksRepo.find({
-      order: { 
-      id: 'ASC'
-    }});
-    res.status(200).json({ booksArray: result, sortOptions, genres });
+  const queryKeys = Object.keys(req.query);
+  const isExistAnotherValues =
+    !queryKeys.length || (queryKeys.length === 1 && "page" in req.query);
+
+  const take = 12;
+  const skip = typeof parseInt(page) === "number" ? take * parseInt(page) : 0;
+
+  const booksArray = booksRepo.createQueryBuilder("book");
+  if (isExistAnotherValues) {
+    const [result, count] = await booksArray
+      .skip(skip)
+      .take(take)
+      .getManyAndCount();
+    const pageCount = Math.ceil(count / take);
+
+    res
+      .status(200)
+      .json({ booksArray: result, pageCount, sortOptions, genres });
     return;
   }
 
-  const booksArray = booksRepo.createQueryBuilder("book");
-  const genresList = genre ? genre.split(',') : [];
-    if (genresList.length) {
-      booksArray.where("book.genre IN (:...genres)", {genres: genresList});
-    };
-    if (minPrice) {
-      booksArray.where("book.price >= :price", { price: minPrice });
-    };
-    if (maxPrice) {
-      booksArray.where("book.price <= :price", { price: maxPrice });
-    };
-    if (sortOption) {
-      booksArray.orderBy(`book.${sortOption}`, "ASC");
-    };
-    const result = await booksArray.getMany()
-  res.status(200).json({ booksArray: result, sortOptions, genres });
-}
+  const genresList = genre ? genre.split(",") : [];
+  if (genresList.length > 0) {
+    booksArray.where("book.genre IN (:...genres)", { genres: genresList });
+  }
+  if (minPrice) {
+    booksArray.where("book.price >= :price", { price: minPrice });
+  }
+  if (maxPrice) {
+    booksArray.where("book.price <= :price", { price: maxPrice });
+  }
+  if (sortOption) {
+    booksArray.orderBy(`book.${sortOption}`, "ASC");
+  }
+
+  const [result, count] = await booksArray
+    .skip(skip)
+    .take(take)
+    .getManyAndCount();
+  const pageCount = Math.ceil(count / take);
+  res.status(200).json({ booksArray: result, pageCount, sortOptions, genres });
+};
